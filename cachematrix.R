810645ee06b75590b9c3d4b9063e4a83579aa669
Mary McDonald <mcdonald.e.mary@gmail.com>
2015-01-24 22:11:11 -0500

    Add cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a pair of functions that cache the inverse of
+## a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object: 
+##it is a list containing a function to
+##set the value of the matrix, get the value of the matrix
+##set the value of the inverse, and then get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        m<-NULL
+        set<-function(y){
+          x<<-y
+          m<<-NULL
+        }
+        
+        get<-function() x
+        setinverse<-function(solve)
+          m<<-solve
+        getinverse<-function() m
+        list(set=set, get=get, 
+             setinverse=setinverse
+             getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the matrix object returned by makeCacheMatrix
+## If the inverse has already been calculated (and the matrix hasn't changed)
+## cacheSolve will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+      m<-x$getinverse()
+      if(!is.null(m)){
+        message("getting cached data")
+        return (m)
+      }
+      data<-x$get()
+      m<-solve(data,...)
+      x$setinverse(m)
+      m
 }
+
+##I thought at first that the assignment was to write an algorithm calculating the inverse
+##of a matrix. After I'd written it, I read over the assignment and saw that I was 
+##wrong. Anyway, if you want to see my inverse algorithm, it's here:
+##https://github.com/marymcdonald/inversematrix/blob/master/inversematrixalg
+## It ends up being more accurate than solve, but I didn't try it out with massive matrices
+## so it is most likely slower...
